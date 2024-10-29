7b8749221a176cf4cdad06cd3298dc0184315469
Jiahao-Zeng <157934567+Jiahao-Zeng@users.noreply.github.com>
2024-10-29 12:58:24 -0400

    asmd

---
+++ src/ASMD.java
@@ -0,0 +1,19 @@
+// import java.util.Scanner;
+
+public class ASMD {
+
+
+    public static double add(double a, double b) {
+        return a + b;
+    }
+    public static double subtract(double a, double b) {
+        return a - b;
+    }
+    public static double multiply(double a, double b) {
+        return a * b;
+    }
+    public static double divide(double a, double b) {
+        return a / b;
+    }
+
+}
--- src/Calculator.java
+++ src/Calculator.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
+import java.lang.Math;
 
 public class Calculator {
     public static void main(String[] args) throws Exception {
         Scanner scan = new Scanner(System.in);
+        System.out.println(Math.sqrt(9));
         scan.close();
     }
 }
